03dae6bb8f94d0ca1e790c666357aeec73870bab
shengbo66 <45621248+shengbo66@users.noreply.github.com>
2019-01-09 02:12:30 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations CICD</h2>
-                <p>Hello AWS v1.9</p>
+                <h1>AWS CodePineline</h1>
+                <h2>Congratulations CICD!</h2>
+                <p>Hello AWS v2.0</p>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
